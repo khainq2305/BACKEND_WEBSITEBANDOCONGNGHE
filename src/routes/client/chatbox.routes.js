856f68d04173691f1656458ac2c73a93a9dae02b
nguyenquockhai <khainqpc08388@gmail.com>
2025-08-24 02:33:51 +0700

    FIX SRC

--- src/routes/client/chatbox.routes.js
+++ src/routes/client/chatbox.routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const chatboxController = require('../../controllers/client/chatboxController');
 
-router.post('/', (req, res) => chatboxController.chat(req, res));
+// chỉ cho phép domain frontend
+const chatboxCors = cors({
+  origin: ["https://www.cyberzone.com.vn", "http://localhost:3000", "http://localhost:5173"],
+  methods: ["POST"],
+});
+
+router.post('/', chatboxCors, (req, res) => chatboxController.chat(req, res));
 
 module.exports = router;
