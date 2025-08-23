749b2c2d2766283302892b483eb96c7e536ca6b2
nguyenquockhai <khainqpc08388@gmail.com>
2025-08-23 19:52:03 +0700

    UPDATED SRC

--- src/routes/client/chatbox.routes.js
+++ src/routes/client/chatbox.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const chatboxController = require('../../controllers/client/chatboxController');
 
-router.post('/', (req, res) => chatboxController.chat(req, res));
+// Cho phép CORS riêng cho route chatbox
+router.post('/', cors(), (req, res) => chatboxController.chat(req, res));
 
 module.exports = router;
