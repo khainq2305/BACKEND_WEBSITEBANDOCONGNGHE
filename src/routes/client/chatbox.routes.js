8e59b1cbe91ed3b53cd8b534df62de9e6650b20b
nguyenquockhai <khainqpc08388@gmail.com>
2025-08-24 17:13:56 +0700

    FIX CHATBOX

--- src/routes/client/chatbox.routes.js
+++ src/routes/client/chatbox.routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const router = express.Router();
 const chatboxController = require('../../controllers/client/chatboxController');
-router.post('/', (req, res) => chatboxController.chat(req, res));
+
+const chatboxCors = cors({
+  origin: [
+    "https://www.cyberzone.com.vn",
+    "http://localhost:3000",
+    "http://localhost:5173"
+  ],
+  methods: ["POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], // 👈 thêm dòng này
+  credentials: true
+});
+
+// Cho phép preflight OPTIONS
+router.options('/', chatboxCors);
+
+// Xử lý chat
+router.post('/', chatboxCors, (req, res) => chatboxController.chat(req, res));
 
 module.exports = router;
