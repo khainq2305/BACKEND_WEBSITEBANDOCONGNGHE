164be475342b505e152af871f78843b229961c10
nguyenquockhai <khainqpc08388@gmail.com>
2025-08-25 14:58:45 +0700

    FIX SRC 1111

--- src/controllers/client/chatboxController.js
+++ src/controllers/client/chatboxController.js
@@ -414,7 +414,7 @@ class ChatboxController {
         /* 2) Lấy dữ liệu */
         const [products, categories, brands] = await Promise.all([
             this.fetchChatProducts({
-                limit: 20,
+                limit: 10,
                 allActiveFlashSaleItemsMap: this.allActiveFlashSaleItemsMap,
                 allActiveCategoryDealsMap: this.allActiveCategoryDealsMap,
             }),
