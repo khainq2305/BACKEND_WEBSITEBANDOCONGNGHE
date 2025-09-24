1f241d75cc7b9cc7b68775ebe57ad28153ecbb53
nguyenquockhai <khainqpc08388@gmail.com>
2025-09-24 18:21:42 +0700

    fix điểm thưởng 2

--- src/controllers/client/cartController.js
+++ src/controllers/client/cartController.js
@@ -552,29 +552,30 @@ class CartController {
       // ----------------------------
       // 3. Lấy tổng điểm hiện tại của user
       // ----------------------------
-      const result = await UserPoint.findOne({
-        attributes: [
-          [
-            sequelize.fn(
-              "COALESCE",
-              sequelize.fn(
-                "SUM",
-                sequelize.literal(`
+     const result = await UserPoint.findOne({
+  attributes: [
+    [
+      sequelize.fn(
+        "COALESCE",
+        sequelize.fn(
+          "SUM",
+          sequelize.literal(`
             CASE
               WHEN type = 'earn' THEN points
-              WHEN type IN ('spend','expired') THEN -points   -- ❌ sửa lại ở đây
+              WHEN type IN ('spend','expired','refund') THEN -points
               ELSE 0
             END
           `)
-              ),
-              0
-            ),
-            "totalPoints",
-          ],
-        ],
-        where: { userId },
-        raw: true,
-      });
+        ),
+        0
+      ),
+      "totalPoints",
+    ],
+  ],
+  where: { userId },
+  raw: true,
+});
+
 
       // ✅ Ép kiểu về số, tránh lỗi chuỗi
       const userPoints = Number(result?.totalPoints) || 0;
